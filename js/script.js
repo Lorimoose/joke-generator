79e8fed52007fc1d95af3509cfd9741de7040548
Lori Moose <lorimoose@Loris-Air.lan>
2023-07-08 08:38:26 -0500

    button now generates new quotes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 const content = document.querySelector("#quote");
 const author = document.querySelector("#author");
+const button = document.querySelector("#regenerate-btn");
+const image = document.querySelector("image");
 
-fetch("https://api.quotable.io/random")
-  .then((response) => {
-    console.log("resolved", response);
-    return response.json();
-  })
-  .then((data) => {
-    content.innerText = `"${data.content}"`;
-    author.innerText = `-${data.author}`;
-  })
-  .catch((err) => {
-    console.log("rejected", err);
-  });
+function newQuote(event) {
+  event.preventDefault();
+  fetch("https://api.quotable.io/random")
+    .then((response) => {
+      console.log("resolved", response);
+      return response.json();
+    })
+    .then((data) => {
+      content.innerText = `"${data.content}"`;
+      author.innerText = `-${data.author}`;
+    })
+    .catch((err) => {
+      console.log("rejected", err);
+    });
+}
+button.addEventListener("click", newQuote);
+
+function newImg() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+      console.log("resolved", response);
+      return response.json();
+    })
+    .then((blob) => {
+      const imageUrl = URL.createObjectURL(blob);
+      image.src = imageUrl;
+    })
+    .catch((err) => {
+      console.log("rejected", err);
+    });
+  }
+button.addEventListener("click", newImg);
