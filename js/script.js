47b45d666889b9c726667291e9c28c52e2535073
Lori Moose <lorimoose@Loris-Air.lan>
2023-07-14 14:51:48 -0500

    added show answer button and event listener to hide and show punchline

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 const setup = document.querySelector("#setup");
 const punchline = document.querySelector("#punchline");
 const button = document.querySelector("#regenerate-btn");
+const answerButton = document.querySelector("#answer-btn");
 const image = document.getElementById('image');
+
+
+answerButton.addEventListener('click', showAnswer)
+
+function showAnswer() {
+    punchline.classList.remove('visually-hidden')
+}
+
+function hideAnswer() {
+  punchline.classList.add('visually-hidden')
+}
+button.addEventListener("click", hideAnswer);
 
 function newJoke(event) {
   event.preventDefault();
@@ -18,7 +31,7 @@ function newJoke(event) {
       console.log("rejected", err);
     });
 }
-button.addEventListener("click", newJoke);
+button.addEventListener("click", newJoke, hideAnswer);
 
 
 function newImg() {
