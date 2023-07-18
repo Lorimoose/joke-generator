b7a4993d986a0c2b73e2d4870cd8dc075fb6d5bc
Lori Moose <lorimoose@Loris-MacBook-Air.local>
2023-07-17 21:05:54 -0500

    fixed dark/light mode button

--- js/script.js
+++ js/script.js
@@ -2,17 +2,16 @@ const setup = document.querySelector("#setup");
 const punchline = document.querySelector("#punchline");
 const button = document.querySelector("#regenerate-btn");
 const answerButton = document.querySelector("#answer-btn");
-const darkModeButton = document.querySelector("#dark-mode-btn");
-const image = document.getElementById('image');
-
+const image = document.getElementById("image");
+const darkModeButton = document.querySelector("#dark-mode-toggle");
 
 function showAnswer() {
-    punchline.classList.remove('visually-hidden')
+  punchline.classList.remove("visually-hidden");
 }
-answerButton.addEventListener('click', showAnswer)
+answerButton.addEventListener("click", showAnswer);
 
 function hideAnswer() {
-  punchline.classList.add('visually-hidden')
+  punchline.classList.add("visually-hidden");
 }
 button.addEventListener("click", hideAnswer);
 
@@ -33,14 +32,13 @@ function newJoke(event) {
 }
 button.addEventListener("click", newJoke);
 
-
 function newImg() {
-  fetch('https://dog.ceo/api/breeds/image/random')
-  .then(response => response.json())
-  .then(response => {
-    console.log("resolved", response)
-    image.src = response.message
-  })
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((response) => {
+      console.log("resolved", response);
+      image.src = response.message;
+    })
     .catch((err) => {
       console.log("rejected", err);
     });
@@ -48,7 +46,10 @@ function newImg() {
 button.addEventListener("click", newImg);
 
 function darkMode() {
-    var element = document.body;
+  let element = document.body;
   element.classList.toggle("dark-mode");
+  if (darkModeButton.innerText == "Dark Mode")
+    darkModeButton.innerText = "Light Mode";
+  else darkModeButton.innerText = "Dark Mode";
 }
-darkModeButton.addEventListener('click', darkMode)
+darkModeButton.addEventListener("click", darkMode);
